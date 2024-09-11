1403d2d4e550d76fd7dd9447c3126a194ee568fd
Lokesh Velayudham - lowkeyy.eth <lokesh.beyondx@gmail.com>
2024-09-10 23:19:22 -0400

    gh-pages

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-<!-- # Slick Portfolio With Svelte.
-
+# Slick Portfolio With Svelte.
+<!-- 
 Vercel-like style portfolio template for developers.
 
 [For a reference, check out my personal protfolio, slightly changed, but uses this template.](https://riadhadrani.github.io/RiadhAdrani)
--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,7 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
-// const base = '/lokeshvelayudham/portfolio';
+const base = '/portfolio';
 
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -25,7 +25,7 @@ const config = {
 			'@utils': './src/lib/utils'
 		},
 		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/portfolio' : ''
+			base: process.env.NODE_ENV === 'production' ? base : ''
 		}
 	}
 };
